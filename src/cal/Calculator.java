79ad37ee53f969020025bc0ed06557d573882062
HuiMing <minghui4-c@.my.cityu.edu.hk>
2014-09-14 01:39:10 +0800

    [Infix2Postfix] convert from infix expression to postfix expression

--- src/cal/Calculator.java
+++ src/cal/Calculator.java
@@ -8,15 +8,22 @@ import java.util.Scanner;
 public class Calculator {
 
   private static String inputExpression;
-  private ExpressionTree expTree;
+  private static ExpressionTree expTree;
   
   public static void main(String[] args) {
     System.out.println("Enter an expression");
     inputExpression = scanInput();
     System.out.println("You entered: " + inputExpression);
+    System.out.println(getPostfixExpression(inputExpression));
   }
   
-  // XXX : need to reflector this method to expends InputHandle
+  private static String getPostfixExpression(String infix) {
+	// TODO Auto-generated method stub
+	expTree = new ExpressionTree();
+	return expTree.parseToPostfix(infix);
+}
+
+// XXX : need to reflector this method to expends InputHandle
   public static String scanInput() {
     Scanner in = new Scanner(System.in);
     return in.nextLine();
