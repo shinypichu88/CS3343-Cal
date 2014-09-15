8e845b9b454749b572ca78918f68f42256bc46bb
Jason Yu <jasonycw1992@gmail.com>
2014-09-15 22:35:42 +0800

    [ADD] Interface for input output

--- src/cal/Calculator.java
+++ src/cal/Calculator.java
@@ -6,27 +6,32 @@ import java.util.Scanner;
  * The Class Calculator.
  */
 public class Calculator {
+	private static InputOutput expressionTree;
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    scanInput();
-  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		scanInput();
+	}
 
-  public static void scanInput() {
-    String s;
-    Scanner in = new Scanner(System.in);
-    System.out.println("Enter an expression:");
-    s = in.nextLine();
-    System.out.println("You entered: " + s);
-  }
+	public static void scanInput() {
+		String s;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter an expression: ");
+		s = in.nextLine();
+		expressionTree = new ExpressionTree();
+		if (expressionTree.input(s))
+			System.out.println("Result:\n" + expressionTree.output());
+	}
 
-  public void setInputExpression(final String string) {
-    // TODO Auto-generated method stub
-  }
+	public void setInputExpression(final String string) {
+		// TODO Auto-generated method stub
+		expressionTree = new ExpressionTree();
+		expressionTree.input(string);
+	}
 
-  public int execute() {
-    // TODO Auto-generated method stub
-    return 0;
-  }
+	public int execute() {
+		// TODO Auto-generated method stub
+		return Integer.parseInt(expressionTree.output());
+	}
 
 }
--- src/cal/ExpressionTree.java
+++ src/cal/ExpressionTree.java
@@ -1,10 +1,22 @@
 package cal;
 
-public class ExpressionTree {
+public class ExpressionTree implements InputOutput{
 
   public String parseToPostfix(String string) {
     // TODO Auto-generated method stub
     return null;
   }
+
+@Override
+public boolean input(String inString) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+@Override
+public String output() {
+	// TODO Auto-generated method stub
+	return "0";
+}
 
 }
