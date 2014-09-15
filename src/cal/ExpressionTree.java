0ec7e4318495713b170bdeab799217de89781898
alan <wkwong337-c@my.cityu.edu.hk>
2014-09-15 23:30:30 +0800

    [New] basic function for building expression tree (not yet implemented)

--- src/cal/ExpressionTree.java
+++ src/cal/ExpressionTree.java
@@ -10,10 +10,28 @@ public class ExpressionTree {
 	public ExpressionTree() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void buildTree(String inputExpression) {
+		
+	}
+	
+	private boolean isOperator(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^' || ch =='('
+        		|| ch == ')';
+    }
+	
+	private void push(TreeNode node) {
+		
+	}
+	
+	private TreeNode pop() {
+		return null;
+	}
+	
 	public int parseResult(String inputExpression) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
 
 }
