d609b308a182ad42f6fc3df314e4ae007e3068bc
Jason Yu <jasonycw1992@gmail.com>
2014-09-17 23:01:32 +0800

    Updated comment Remove un used import

--- src/cal/ExpressionTree.java
+++ src/cal/ExpressionTree.java
@@ -3,7 +3,6 @@ package cal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class ExpressionTree implements InputOutput {
 
@@ -12,36 +11,41 @@ public class ExpressionTree implements InputOutput {
 	private Stack<String> stack;
 	private double result;
 
+	// Constructor
 	public ExpressionTree() {
 		stack = new Stack<String>();
 	}
 
+	// Method to build the tree and calculate the result
 	public void buildTree() {
 		for (int i = 0; i < postfix.size(); i++)
 			insert(postfix.get(i));
 		this.result = evaluate(headTreeNode);
 	}
 
+	// Method to insert node
 	private void insert(String val) {
 		try {
 			if (BasicMathsMethods.isDigit(val)) {
 				stack.push(val);
 			} else if (BasicMathsMethods.isOperator(val)) {
 				Operator operator = new Operator(val.charAt(0));
-				if(headTreeNode == null) {
-					Operand rightOperand = new Operand(Double.parseDouble(stack.pop()));
-					Operand leftOperand = new Operand(Double.parseDouble(stack.pop()));
+				if (headTreeNode == null) {
+					Operand rightOperand = new Operand(Double.parseDouble(stack
+							.pop()));
+					Operand leftOperand = new Operand(Double.parseDouble(stack
+							.pop()));
 					headTreeNode = operator;
 					headTreeNode.left = leftOperand;
 					headTreeNode.right = rightOperand;
-				}  else {
-					Operand rightOperand = new Operand(Double.parseDouble(stack.pop()));
+				} else {
+					Operand rightOperand = new Operand(Double.parseDouble(stack
+							.pop()));
 					operator.left = headTreeNode;
 					operator.right = rightOperand;
 					headTreeNode = operator;
 				}
 
-				
 			}
 		} catch (Exception e) {
 			System.out.println("Invalid Expression" + e);
@@ -53,37 +57,49 @@ public class ExpressionTree implements InputOutput {
 		return Integer.parseInt(output());
 	}
 
-	private double evaluate ( TreeNode node ) { 
-	double result = 0; // Value to be returned 
-	if ( node.left == null && node.right == null ) { // Just get the value of the leaf 
-		Operand op = (Operand)node;
-		result = (double)op.val; 
+	// Calculate the result of the tree
+	private double evaluate(TreeNode node) {
+		double result = 0; // Value to be returned
+		if (node.left == null && node.right == null) { // Just get the value of
+														// the leaf
+			Operand op = (Operand) node;
+			result = (double) op.val;
+		} else {
+			// We've got work to do, evaluating the expression
+			double left, right;
+			Operator opt = (Operator) node;
+			char operator = (char) opt.val;
+			// Capture the values of the left and right subexpressions
+			left = evaluate(node.left);
+			right = evaluate(node.right);
+			// Do the arithmetic, based on the operator
+			switch (operator) {
+			case '+':
+				result = left + right;
+				break;
+			case '-':
+				result = left - right;
+				break;
+			case '*':
+				result = left * right;
+				break;
+			case '/':
+				result = left / right;
+				break;
+			case '^':
+				result = Math.pow(left, right);
+				break;
+			// NOTE: allow fall-through from default to case '+'
+			default:
+				System.out.println("Unrecognized operator " + operator
+						+ " treated as +.");
+
+			}
+		}
+		// Return either the leaf's value or the one we just calculated.
+		return result;
 	}
-	else  { 
-		// We've got work to do, evaluating the expression 
-		double left, right;
-		Operator opt = (Operator)node;
-		char operator = (char)opt.val; 
-		// Capture the values of the left and right subexpressions 
-		left = evaluate ( node.left ); 
-		right = evaluate ( node.right ); 
-		// Do the arithmetic, based on the operator 
-		switch ( operator ) { 
-			case '+': result = left + right; break;
-			case '-': result = left - right; break; 
-			case '*': result = left * right; break; 
-			case '/': result = left / right; break; 
-			case '^': result = Math.pow (left, right ); break; 
-			// NOTE: allow fall-through from default to case '+' 
-			default: System.out.println ("Unrecognized operator " + 
-					operator + " treated as +."); 
-			  
-		 } 
-	 } 
-	 // Return either the leaf's value or the one we just calculated. 
-	 return result; 
-	 } 
-	
+
 	@Override
 	public boolean input(String infixInput) {
 		// TODO Auto-generated method stub
@@ -105,7 +121,7 @@ public class ExpressionTree implements InputOutput {
 
 		ArrayList<String> resultList = new ArrayList<String>();
 		input = input.replaceAll("\\s+", " ");
-//		System.out.println(input.trim());
+		// System.out.println(input.trim());
 		String[] resultArray = input.trim().split(" ");
 		Collections.addAll(resultList, resultArray);
 		return resultList;
