aee64054fb177f11985f6e82b519d94c883c42c2
Jason Yu <jasonycw1992@gmail.com>
2014-09-17 21:43:46 +0800

    Add loop for inputting

--- src/cal/Calculator.java
+++ src/cal/Calculator.java
@@ -15,19 +15,25 @@ public class Calculator {
 	}
 
 	public static void scanInput() {
-		String s;
+		String s = "";
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter an expression: ");
-		s = in.nextLine();
-		if (inputHandler.setInput(s))
-			System.out.println("Result:\n" + inputHandler.output());
-		else
-			System.out.println("Improper input format.");
+		while (true) {
+			System.out.print("Enter: ");
+			s = in.nextLine();
+			if(s.toLowerCase().equals("quit"))
+				break;
+			if (inputHandler.setInput(s))
+				System.out.println("Result:\n" + inputHandler.output());
+			else
+				System.out.println("Improper input format.");
+			System.out.println();
+		}
+		System.out.println("Bye!");
 	}
 
 	public void setInputExpression(final String string) {
 		// TODO Auto-generated method stub
-		inputHandler  = new InputHandler();
+		inputHandler = new InputHandler();
 		inputHandler.setInput(string);
 	}
 
