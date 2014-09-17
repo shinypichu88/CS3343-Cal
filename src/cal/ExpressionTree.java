13e71047bc2bf6c190cd40fc40985e0de6bf5180
Jason Yu <jasonycw1992@gmail.com>
2014-09-17 14:01:25 +0800

    Make function for create postfix ArrayList Input infix String: 1+(2.2+333)*444 Postfix ArrayList: [1, 2.2, 333, +, 444, *, +]

--- src/cal/ExpressionTree.java
+++ src/cal/ExpressionTree.java
@@ -1,12 +1,15 @@
 package cal;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class ExpressionTree implements InputOutput {
 
 	private TreeNode headTreeNode;
-	private String posfixInput;
+	private String postfix;
+	private ArrayList<String> posfixInput;
 	private Stack<Character> stack;
 
 	public ExpressionTree() {
@@ -24,7 +27,7 @@ public class ExpressionTree implements InputOutput {
 		try {
 			if (isDigit(val)) {
 				stack.push(val);
-			} else if (isOperator(val)) {
+			} else if (isOperator(String.valueOf(val))) {
 				Operator operator = new Operator(val);
 				Operand operand = new Operand(stack.pop());
 
@@ -47,8 +50,8 @@ public class ExpressionTree implements InputOutput {
 		return ch >= '0' && ch <= '9';
 	}
 
-	private boolean isOperator(char ch) {
-		return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
+	private boolean isOperator(String currentChar) {
+		return currentChar.equals("+") || currentChar.equals("-") || currentChar.equals("*") || currentChar.equals("/") || currentChar.equals("^");
 	}
 
 	public int parseResult(String inputExpression) {
@@ -59,37 +62,87 @@ public class ExpressionTree implements InputOutput {
 	@Override
 	public boolean input(String infixInput) {
 		// TODO Auto-generated method stub
-		posfixInput = toPostfix(infixInput);
+		String input = infixInput.replace(" ", "");
+		posfixInput = toPostfix(toStringArray(input));
+		postfix = toPostfix(input);
 		return true;
 	}
 
+	private ArrayList<String> toStringArray(String input) {
+		input = input.replace("(", " ( ");
+		input = input.replace(")", " ) ");
+		input = input.replace("+", " + ");
+		input = input.replace("-", " - ");
+		input = input.replace("*", " * ");
+		input = input.replace("/", " / ");
+		input = input.replace("^", " ^ ");
+		
+		ArrayList<String> resultList = new ArrayList<String>();
+		input = input.replaceAll("\\s+", " ");
+		String[] resultArray = input.split(" ");
+		Collections.addAll(resultList, resultArray);
+		return resultList;
+	}
+
 	// Check if operator 2 is higher priority than operator 1
-	private boolean comparePriority(char operator1, char operator2) {
-		if ((operator2 == '+' || operator2 == '-') && (operator1 == '+' || operator1 == '-'))
+	private boolean comparePriority(String operator1, String operator2) {
+		if ((operator2.equals("+") || operator2.equals("-")) && (operator1.equals("+") || operator1.equals("-")))
 			return true;
-		else if ((operator2 == '*' || operator2 == '/')
-				&& (operator1 == '+' || operator1 == '-' || operator1 == '*' || operator1 == '/'))
+		else if ((operator2.equals("*") || operator2.equals("/"))
+				&& (operator1.equals("+") || operator1.equals("-") || operator1.equals("*") || operator1.equals("/")))
 			return true;
-		else if ((operator2 == '^')
-				&& (operator1 == '+' || operator1 == '-' || operator1 == '*' || operator1 == '/'))
+		else if ((operator2.equals("^"))
+				&& (operator1.equals("+") || operator1.equals("-") || operator1.equals("*") || operator1.equals("/")))
 			return true;
 		else
 			return false;
 	}
 
 	// Convert infix to postfix: (1*2)+3 -> 12*3+
+	private ArrayList<String> toPostfix(ArrayList<String> arrayList) {
+		ArrayList<String> postfix = new ArrayList<String>(); // equivalent postfix is empty initially
+		Stack<String> operatorStack = new Stack<>(); // stack to hold symbols
+		operatorStack.push("#"); // symbol to denote end of stack
+		for (int i = 0; i < arrayList.size(); i++) {
+			String currentChar = arrayList.get(i);
+			// if a operator repeatedly pops if stack top has same or higher precedence
+			if (isOperator(currentChar)) {
+				while (comparePriority(currentChar, operatorStack.peek()))
+					postfix.add(operatorStack.pop());
+				operatorStack.push(currentChar);
+			}
+			// push if left parenthesis
+			else if (currentChar.equals("("))
+				operatorStack.push(currentChar);
+			else if (currentChar.equals(")")) {
+				// repeatedly pops if right parenthesis until left parenthesis is found
+				while (!operatorStack.peek().equals("("))
+					postfix.add(operatorStack.pop());
+				operatorStack.pop();
+			} else
+				postfix.add(currentChar);
+		}
+		// pops all elements of stack left
+		while (!operatorStack.peek().equals("#")) {
+			postfix.add(operatorStack.pop());
+		}
+
+		return postfix;
+	}
+	
 	private String toPostfix(String infix) {
 		String postfix = ""; // equivalent postfix is empty initially
-		Stack<Character> operatorStack = new Stack<Character>(); // stack to hold symbols
+		Stack<Character> operatorStack = new Stack<>(); // stack to hold symbols
 		operatorStack.push('#'); // symbol to denote end of stack
 		for (int i = 0; i < infix.length(); i++) {
-			char currentChar = infix.charAt(i);
-			if (isOperator(currentChar)) {// if a operator repeatedly pops if stack top has same or higher precedence
-				while (comparePriority(currentChar, operatorStack.peek()))
+			char currentChar = infix.charAt(i);// symbol to be processed
+			if (isOperator(String.valueOf(currentChar))) {// if a operator
+				// repeatedly pops if stack top has same or higher precedence
+				while (comparePriority(String.valueOf(currentChar), String.valueOf(operatorStack.peek())))
 					postfix += operatorStack.pop();
 				operatorStack.push(currentChar);
-			} else if (currentChar == '(')// push if left parenthesis
-				operatorStack.push(currentChar);
+			} else if (currentChar == '(')
+				operatorStack.push(currentChar);// push if left parenthesis
 			else if (currentChar == ')') {
 				// repeatedly pops if right parenthesis until left parenthesis is found
 				while (operatorStack.peek() != '(')
@@ -109,7 +162,8 @@ public class ExpressionTree implements InputOutput {
 	@Override
 	public String output() {
 		// TODO Auto-generated method stub
-		return posfixInput;
+		return posfixInput.toString();
+//		return postfix;
 	}
 
 }
