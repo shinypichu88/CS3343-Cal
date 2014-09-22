af0dc24f9b6c6909fd6068f5ae291a9f7428e3f9
Jason Yu <jasonycw1992@gmail.com>
2014-09-22 15:12:33 +0800

    Add javadoc to BasicMathsMethods

--- src/cal/BasicMathsMethods.java
+++ src/cal/BasicMathsMethods.java
@@ -1,23 +1,54 @@
 package cal;
 
+/**
+ * This is a class to handle the common mathematics method.
+ */
 public class BasicMathsMethods {
+	
+	/**
+	 * Checks if the string is digit.
+	 *
+	 * @param str The input string
+	 * @return true, if is digit
+	 */
 	public static boolean isDigit(String str) {
 		return str.matches("\\d+(\\.\\d+)?");
 	}
 
-	// Check if the input string is an operator
+	/**
+	 * Checks if the character is an operator.
+	 *
+	 * @param chr The input Character
+	 * @return true, if is operator
+	 */
 	public static boolean isOperator(String chr) {
 		return chr.equals("+") || chr.equals("-") || chr.equals("*")
 				|| chr.equals("/") || chr.equals("^");
 	}
 
-	// Check if the input character is an operator
+	/**
+	 * Checks if the string is an operator.
+	 *
+	 * @param chr The input String
+	 * @return true, if is operator
+	 */
 	public static boolean isOperator(char chr) {
 		return chr == '+' || chr == '-' || chr == '*' || chr == '/'
 				|| chr == '^';
 	}
 
-	// Check if operator 2 is higher priority than operator 1
+	/**
+	 * Compare the priority of the 2 operator.
+	 * <ol>
+	 * <li>^
+	 * <li>*, /
+	 * <li>+, *
+	 * </ol>
+	 *
+	 * @param operator1 The 1st operator
+	 * @param operator2 The 2nd operator
+	 * @return true, if operator 1 has higher priority than operator 2
+	 */
 	public static boolean comparePriority(String operator1, String operator2) {
 		if ((operator2.equals("+") || operator2.equals("-"))
 				&& (operator1.equals("+") || operator1.equals("-")))
