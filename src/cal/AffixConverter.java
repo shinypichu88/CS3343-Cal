55374e86a3e1eacb165bb4ceb02957c603d2def7
Jason Yu <jasonycw1992@gmail.com>
2014-09-22 11:22:46 +0800

    Add more comment to AffixConverter

--- src/cal/AffixConverter.java
+++ src/cal/AffixConverter.java
@@ -3,14 +3,21 @@ package cal;
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * This class handles the conversion between difference affix.
+ * 
+ * @author Jason Yu
+ */
 public class AffixConverter {
 	/**
 	 * Convert infix to postfix expression: 1*(22+3.33) -> 1 22 3.33 + *
+	 * <ul>
+	 * <li>Input: ArrayList<String> ["1","*","(","22","+","3.33",")"]
+	 * <li>Output: ArrayList<String> ["1","22","3.33","+","*"]
+	 * </ul>
 	 * 
-	 * Input: ArrayList<String>  ["1","*","(","22","+","3.33",")"]
-	 * Output: ArrayList<String>  ["1","22","3.33","+","*"]
-	 *
-	 * @param ArrayList of a infix expression
+	 * @param arrayList
+	 *            an ArrayList of a infix expression
 	 * @return ArrayList of a postfix expression
 	 */
 	public static ArrayList<String> toPostfix(ArrayList<String> arrayList) {
@@ -23,7 +30,8 @@ public class AffixConverter {
 			// if a operator repeatedly pops if stack top has same or higher
 			// precedence
 			if (BasicMathsMethods.isOperator(currentChar)) {
-				while (BasicMathsMethods.comparePriority(currentChar, operatorStack.peek()))
+				while (BasicMathsMethods.comparePriority(currentChar,
+						operatorStack.peek()))
 					postfix.add(operatorStack.pop());
 				operatorStack.push(currentChar);
 			}
