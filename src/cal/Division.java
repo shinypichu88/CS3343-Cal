afe68a058f03919fecc527ebe70c43a8cf6cea01
Lewis Ho <lewishoch@gmail.com>
2014-10-27 22:26:36 +0800

    Add java doc to Division class

--- src/cal/Division.java
+++ src/cal/Division.java
@@ -1,11 +1,40 @@
 package cal;
 
-public class Division extends Operator{
+/**
+ * 
+ * This is a concrete
+ * class that is used 
+ * to representing 
+ * a division object
+ * which is extending the
+ * abstract operator class
+ * 
+ * @author LewisHo
+ *
+ */
 
+
+public class Division extends Operator{
+	
+	/**
+	 * 
+	 *  A constructor to 
+	 *  initialize the attributes
+	 *  
+	 */
 	public Division() {
 		super("/");
 	}
 
+	/**
+	 * 
+	 *  A method to accept 2 double value
+	 *  calculate and return the division 
+	 *  value
+	 *  @param val1
+	 *  @param val2
+	 *  
+	 */
 	@Override
 	public double calculate(double val1, double val2) {
 		return val1 / val2;
