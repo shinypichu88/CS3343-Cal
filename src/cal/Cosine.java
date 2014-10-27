1ae7765b37c7f1d2b3292713543e7978739b7708
HuiMing <minghui4-c@.my.cityu.edu.hk>
2014-10-27 22:50:45 +0800

    [Add] Java Doc for TrigoOperator, Sine, Cosine [Still Working]

--- src/cal/Cosine.java
+++ src/cal/Cosine.java
@@ -1,10 +1,31 @@
 package cal;
 
+/**
+ * The Class represents Cosine operator
+ * It extends the TrigoOperator
+ * @author Cal
+ *
+ */
 public class Cosine extends TrigoOperator{
+	
+	/**
+	 * Constructor for Cosine Operator
+	 * Initialize by passing "cos" to TrigoOperator to let it know it is a Sine operator
+	 * 
+	 */
 	public Cosine() {
 		super("cos");	
 	}
 	
+	
+	/**
+	 * Calculate the Sine function by calling Math.sin
+	 * @param val1 It is an empty number for design purpose 
+	 * @param val2 It is a input parameter for sine function
+	 * 
+	 * @return Math.sin(val2) It returns a output of Sine functin
+ 	 * 
+	 */
 	@Override
 	public double calculate(double val1, double val2) {
 		return Math.cos(val2);
