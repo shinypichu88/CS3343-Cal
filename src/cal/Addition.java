41ccf915e4cd657a85850542b5924bdc44e9ca4a
Lewis Ho <lewishoch@gmail.com>
2014-10-27 22:49:25 +0800

    Add java doc to Addition class

--- src/cal/Addition.java
+++ src/cal/Addition.java
@@ -1,11 +1,37 @@
 package cal;
-
+/**
+ * 
+ * This is a concrete
+ * class that is used 
+ * to representing 
+ * a Addition object
+ * which is extending the
+ * abstract operator class
+ * 
+ * @author cal
+ *
+ */
 public class Addition extends Operator {
-
+	/**
+	 * 
+	 *  A constructor to 
+	 *  initialize the attributes 
+	 *  using symbol + as Addition
+	 *  
+	 */
 	public Addition() {
 		super("+");
 	}
 
+	/**
+	 * 
+	 *  A method accepting 2 double value
+	 *  to calculate and return the Addition 
+	 *  value
+	 *  @param val1
+	 *  @param val2
+	 *  
+	 */
 	@Override
 	public double calculate(double val1, double val2) {
 		return val1 + val2;
